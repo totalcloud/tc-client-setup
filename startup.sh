997803aedd20d9d2110d06e38b754331ec425577
Shubham Prasad <shubham@totalcloud.io>
2020-05-22 10:44:08 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,8 +8,9 @@ redis:latest
 sleep 5
 
 docker run --rm -it \
---env-file .env \
+--env-file $HOME/env.list \
 --network host \
 -v $HOME/.ssh:/usr/src/app/secrets/ssh \
+-v $HOME/aws:/usr/src/app/secrets/aws \
 --name tc-client \
 totalcloud/tc-agent:latest
